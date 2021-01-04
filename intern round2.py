bb93861a242320686c5a98a6953bf23f911b7411
Sam Richard <josephiterichard0411@gmail.com>
2021-01-05 00:08:21 +0530

    importing

--- intern round2.py
+++ intern round2.py
@@ -1,15 +1,3 @@
 import requests
 from bs4 import BeautifulSoup
 urls=["  https://www.youtube.com/watch?v=fRh_vgS2dFE " , "https://www.youtube.com/watch?v=k2qgadSvNyU"]
-for url in urls:
-    def scrape_info(url):
-        r = BeautifulSoup(requests.get(url).text, "html.parser")#prints the data
-        viewsd=r.select_one('meta[itemprop="interactionCount"][content]')['content']
-        viewss=r.select_one('meta[itemprop="interactionCount"][content]')['content']
-        data_viewsd={viewsd}
-        data_viewss={viewss}
-        return data_viewss
-    if __name__=="__main__":
-        data_views = scrape_info(url)
-        print(data_views)
-    
