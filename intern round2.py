6e55bf31ae95226adae65ade2e127a2f4a1cab88
Sam Richard <josephiterichard0411@gmail.com>
2021-01-05 00:14:18 +0530

    updated

--- intern round2.py
+++ intern round2.py
@@ -3,4 +3,5 @@ from bs4 import BeautifulSoup
 urls=["  https://www.youtube.com/watch?v=fRh_vgS2dFE " , "https://www.youtube.com/watch?v=k2qgadSvNyU"]
 for url in urls:
     def scrape_info(url):
-      
+        r = BeautifulSoup(requests.get(url).text, "html.parser")
+    
