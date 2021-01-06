66db99371c4b7a8df8d4681210d49e6071351cc0
Sam Richard <josephiterichard0411@gmail.com>
2021-01-06 23:44:04 +0530

    url & for loop

--- intern round2.py
+++ intern round2.py
@@ -4,7 +4,9 @@ import influxdb
 from influxdb import InfluxDBClient
 from datetime import datetime
 from bs4 import BeautifulSoup
-
+urls=["  https://www.youtube.com/watch?v=fRh_vgS2dFE " , "https://www.youtube.com/watch?v=k2qgadSvNyU"]
+for url in urls:
+    
 
 
 
