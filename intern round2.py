4005f63e5529003ffb49b0ed2e549c56c1ed24f0
Sam Richard <josephiterichard0411@gmail.com>
2021-01-06 23:47:17 +0530

    scrapeing data title

--- intern round2.py
+++ intern round2.py
@@ -6,7 +6,10 @@ from datetime import datetime
 from bs4 import BeautifulSoup
 urls=["  https://www.youtube.com/watch?v=fRh_vgS2dFE " , "https://www.youtube.com/watch?v=k2qgadSvNyU"]
 for url in urls:
-    
+    def scrape_info(url):
+        r = BeautifulSoup(requests.get(url).text, "html.parser")
+        title1=r.select_one('meta[itemprop="name"][content]')['content']
+        
 
 
 
